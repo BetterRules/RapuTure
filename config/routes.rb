b6303234d1b20705765ab3c8b21d10ccfa08205e
Brenda Wallace <brenda.wallace@dia.govt.nz>
2018-11-14 14:43:49 +1300

    Style fix up

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@
 
 Rails.application.routes.draw do
   resources :entities
-  resources :variables, only: [:show, :index]
+  resources :variables, only: %i[show index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
