f3af37b255a8666018dd1759067f3badd4780628
Brenda Wallace <brenda.wallace@dia.govt.nz>
2018-11-15 09:11:02 +1300

    Added root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root :to => "variables#index"
   resources :variables, only: %i[show index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
