e270d23377263d1406192e8ad6e27aab3c13a22f
Brenda Wallace <brenda.wallace@dia.govt.nz>
2018-11-15 09:38:31 +1300

    Also link double quoted variables

--- app/helpers/variables_helper.rb
+++ app/helpers/variables_helper.rb
@@ -4,6 +4,7 @@ module VariablesHelper
   def display_formula(formula)
     Variable.all.each do |v|
       formula.gsub!("'#{v.name}'", "<strong>'#{link_to(v.name, v)}'</strong>")
+      formula.gsub!("\"#{v.name}\"", "<strong>'#{link_to(v.name, v)}'</strong>")
     end
     formula.html_safe
   end
