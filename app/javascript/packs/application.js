c5c1378ea5f4ad19b1c059c338b0066037546f46
Brenda Wallace <brenda.wallace@dia.govt.nz>
2018-12-21 15:34:57 +1300

    Revert "Auto corrected by following Javascript quotes"
    This reverts commit 19bf67cdb4e93493baa52006c173aa4567ff3b0c.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,6 @@
 
 console.log('Hello World from Webpacker');
 // Support component names relative to this directory:
-let componentRequireContext = require.context('components', true);
-let ReactRailsUJS = require('react_ujs');
-
-ReactRailsUJS.useContext(componentRequireContext);
+var componentRequireContext = require.context("components", true)
+var ReactRailsUJS = require("react_ujs")
+ReactRailsUJS.useContext(componentRequireContext)
--- app/javascript/packs/server_rendering.js
+++ app/javascript/packs/server_rendering.js
@@ -1,6 +1,5 @@
 // By default, this pack is loaded for server-side rendering.
 // It must expose react_ujs as `ReactRailsUJS` and prepare a require context.
-let componentRequireContext = require.context('components', true);
-let ReactRailsUJS = require('react_ujs');
-
-ReactRailsUJS.useContext(componentRequireContext);
+var componentRequireContext = require.context("components", true)
+var ReactRailsUJS = require("react_ujs")
+ReactRailsUJS.useContext(componentRequireContext)
