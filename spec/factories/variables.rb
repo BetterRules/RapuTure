e248e3b9b3d142d074c6f2723c2b805c69b8eac9
Mischa Saunders <mischa.saunders@gmail.com>
2019-01-30 14:42:12 +1300

    merge

---
+++ spec/factories/variables.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :variable do
+    name { Faker::Lorem.word }
+    description { Faker::Lorem.paragraph }
+    href { Faker::Internet.url }
+    spec { '{}' }
+    created_at { DateTime.now.utc }
+    updated_at { DateTime.now.utc }
+    namespace { Faker::Lorem.word }
+    value_type
+    entity
+    references { [] }
+    unit { nil }
+  end
+end
