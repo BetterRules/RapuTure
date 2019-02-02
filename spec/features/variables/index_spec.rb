4d246b7ec1bc0da522144180db6da2170c04dc14
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-02-02 23:24:36 +1300

    Feature specs for badges shown on variables

---
+++ spec/features/variables/index_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature "index" do
+  context "Variable not refered to" do
+    before { FactoryBot.create :variable, name: 'likes_to_eat_chocolate' }
+    describe 'view all variables' do
+      before { visit variables_path }
+      it { expect(page).to have_text "Available filters for Namespace" }
+      it { expect(page).to have_link "likes_to_eat_chocolate" }
+      it { expect(page).to have_text "used 0 times" }
+      it { expect(page).to have_text "Orphaned" }
+    end
+  end
+
+  context "Variable refers to others" do
+    before do 
+      parent = FactoryBot.create :variable, name: 'likes_to_eat_chocolate'
+      child = FactoryBot.create :variable, name: 'likes_to_eat'
+      parent.variables << child
+    end
+    describe 'view all variables' do
+      before { visit variables_path }
+      it { expect(page).to have_link "likes_to_eat_chocolate" }
+      it { expect(page).to have_link "likes_to_eat" }
+      it { expect(page).not_to have_text "Orphaned" }
+      it { expect(page).to have_text "used 1 time" }
+    end
+  end
+end
