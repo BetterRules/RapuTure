72768ae37b73e4aec7e4f201968bbb2efb1c7767
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-02-04 15:11:47 +1300

    Rubocop tidy up

--- spec/controllers/variables_controller_spec.rb
+++ spec/controllers/variables_controller_spec.rb
@@ -4,7 +4,6 @@ require 'rails_helper'
 
 RSpec.describe VariablesController, type: :controller do
   describe '#index' do
-
     context 'two variables that refer to each other' do
       let!(:parent) { FactoryBot.create :variable, name: 'likes_to_eat_chocolate', variables: [child] }
       let!(:child) { FactoryBot.create :variable, name: 'likes_to_eat' }
@@ -17,8 +16,8 @@ RSpec.describe VariablesController, type: :controller do
     context 'one orphaned variable' do
       let!(:variable) { FactoryBot.create :variable }
       before { get :index, params: {} }
-      it { expect(assigns(:link_from_counts)).to eq ({ }) }
-      it { expect(assigns(:link_to_counts)).to eq ({ }) }
+      it { expect(assigns(:link_from_counts)).to eq ({}) }
+      it { expect(assigns(:link_to_counts)).to eq ({}) }
     end
   end
 end
