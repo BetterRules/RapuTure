8daec14c40ed4087abae3987389bad1c9bf49e7b
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-02-04 16:00:06 +1300

    Move specs for more cmplicated scenarios

--- spec/controllers/variables_controller_spec.rb
+++ spec/controllers/variables_controller_spec.rb
@@ -4,6 +4,22 @@ require 'rails_helper'
 
 RSpec.describe VariablesController, type: :controller do
   describe '#index' do
+    context 'one orphaned variable' do
+      let!(:variable) { FactoryBot.create :variable }
+      before { get :index, params: {} }
+      it { expect(assigns(:link_from_counts)).to eq ({}) }
+      it { expect(assigns(:link_to_counts)).to eq ({}) }
+    end
+
+    context 'three orphaned variables' do
+      let!(:variable) { FactoryBot.create :variable }
+      let!(:variable_two) { FactoryBot.create :variable }
+      let!(:variable_three) { FactoryBot.create :variable }
+      before { get :index, params: {} }
+      it { expect(assigns(:link_from_counts)).to eq ({}) }
+      it { expect(assigns(:link_to_counts)).to eq ({}) }
+    end
+
     context 'two variables that refer to each other' do
       let!(:parent) { FactoryBot.create :variable, name: 'likes_to_eat_chocolate', variables: [child] }
       let!(:child) { FactoryBot.create :variable, name: 'likes_to_eat' }
@@ -13,11 +29,20 @@ RSpec.describe VariablesController, type: :controller do
       it { expect(assigns(:link_to_counts)).to eq ({ parent.id => 1 }) }
     end
 
-    context 'one orphaned variable' do
-      let!(:variable) { FactoryBot.create :variable }
+    context 'Many variables that refer to each other' do
+      let!(:parent_1) { FactoryBot.create :variable, name: 'likes_to_eat_chocolate', variables: [child_1] }
+      let!(:child_1) { FactoryBot.create :variable, name: 'likes_to_eat' }
+
+      let!(:parent_2) { FactoryBot.create :variable, name: 'likes_to_drink_water', variables: [child_2] }
+      let!(:child_2) { FactoryBot.create :variable, name: 'likes_to_drink' }
+
       before { get :index, params: {} }
-      it { expect(assigns(:link_from_counts)).to eq ({}) }
-      it { expect(assigns(:link_to_counts)).to eq ({}) }
+
+      # These appear in alphabetical order
+      it { expect(assigns(:variables)).to eq [child_2, parent_2, child_1, parent_1] }
+      
+      it { expect(assigns(:link_from_counts)).to eq ({ child_1.id => 1, child_2.id => 1 }) }
+      it { expect(assigns(:link_to_counts)).to eq ({ parent_1.id => 1, parent_2.id => 1 }) }
     end
   end
 end
