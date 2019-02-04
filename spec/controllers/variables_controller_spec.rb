ebe1a7926cba93772a38f63ae5e5ba74c4cff67c
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-02-04 13:44:11 +1300

    Controller tests for variables#index

--- spec/controllers/variables_controller_spec.rb
+++ spec/controllers/variables_controller_spec.rb
@@ -3,4 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe VariablesController, type: :controller do
+  let!(:parent) { FactoryBot.create :variable, name: 'likes_to_eat_chocolate', variables: [child] }
+  let!(:child) { FactoryBot.create :variable, name: 'likes_to_eat' }
+  describe '#index' do
+    before { get :index, params: {} }
+    it { expect(assigns(:variables)).to eq [child, parent] }
+    it { expect(assigns(:link_from_counts)).to eq ({ child.id => 1 }) }
+    it { expect(assigns(:link_to_counts)).to eq ({ parent.id => 1 }) }
+  end
 end
