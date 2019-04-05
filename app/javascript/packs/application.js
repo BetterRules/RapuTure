77dea0f98c8608c4fc6da6a5fb7f60e025f23314
Awesome Code <team@xinminlabs.com>
2019-04-05 22:39:41 +0000

    Auto corrected by following Lint Javascript prefer-const

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,6 @@
 
 console.log('Hello World from Webpacker');
 // Support component names relative to this directory:
-let componentRequireContext = require.context('components', true);
-let ReactRailsUJS = require('react_ujs');
+const componentRequireContext = require.context('components', true);
+const ReactRailsUJS = require('react_ujs');
 ReactRailsUJS.useContext(componentRequireContext);
--- app/javascript/packs/server_rendering.js
+++ app/javascript/packs/server_rendering.js
@@ -1,5 +1,5 @@
 // By default, this pack is loaded for server-side rendering.
 // It must expose react_ujs as `ReactRailsUJS` and prepare a require context.
-let componentRequireContext = require.context('components', true);
-let ReactRailsUJS = require('react_ujs');
+const componentRequireContext = require.context('components', true);
+const ReactRailsUJS = require('react_ujs');
 ReactRailsUJS.useContext(componentRequireContext);
