0ee92de142bb8d95629b833f157c7ce80037f737
Dana Iti <dana.iti.work@gmail.com>
2019-04-16 16:06:58 +1200

    remove object

--- app/services/github_scrape_service.rb
+++ app/services/github_scrape_service.rb
@@ -4,10 +4,9 @@ class GithubScrapeService
 
   def self.scrape_all
     repos = Github::Client::Repos.new
-    contents = repos.contents user: 'ServiceInnovationLab', repo: 'openfisca-aotearoa'
-
-    directories = contents.get user: 'ServiceInnovationLab', repo: 'openfisca-aotearoa', path: 'openfisca_aotearoa/tests'
-
+    directories = repos.contents.get user: 'ServiceInnovationLab',
+      repo: 'openfisca-aotearoa',path: 'openfisca_aotearoa/tests'
+      
     directories.each do |t|
       puts "======================================================================"
       puts t.url
