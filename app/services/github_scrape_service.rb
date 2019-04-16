079f2f86f1839843f7a46886a344406a039123cb
Dana Iti <dana.iti.work@gmail.com>
2019-04-16 16:26:06 +1200

    get files and dir names

--- app/services/github_scrape_service.rb
+++ app/services/github_scrape_service.rb
@@ -2,13 +2,13 @@
 
 class GithubScrapeService
 
-  def self.connect(dir)
+  def self.connect(dir="")
     repos = Github::Client::Repos.new
-    repos.contents.get user: 'ServiceInnovationLab', repo: 'openfisca-aotearoa', path: "openfisca_aotearoa/#{dir}"
+    repos.contents.get user: 'ServiceInnovationLab', repo: 'openfisca-aotearoa', path: "openfisca_aotearoa/tests/#{dir}"
   end
 
   def self.scrape_all
-    dirs = connect('tests')
+    dirs = connect()
 
     dirs.each do |t|
       puts "======================================================================"
@@ -17,7 +17,7 @@ class GithubScrapeService
         puts "File: #{t.name}"
       else
         puts "Directory: #{t.name}"
-        children = connect("tests/#{t.name}")
+        children = connect(t.name)
         children.each do |c|
           puts c.name
         end
