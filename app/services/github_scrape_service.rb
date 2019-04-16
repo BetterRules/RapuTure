110d6f54985a497486785ed005fefd42b5149b7f
Dana Iti <dana.iti.work@gmail.com>
2019-04-16 16:19:21 +1200

    get files and directory names

--- app/services/github_scrape_service.rb
+++ app/services/github_scrape_service.rb
@@ -2,14 +2,26 @@
 
 class GithubScrapeService
 
-  def self.scrape_all
+  def self.connect(dir)
     repos = Github::Client::Repos.new
-    directories = repos.contents.get user: 'ServiceInnovationLab',
-      repo: 'openfisca-aotearoa',path: 'openfisca_aotearoa/tests'
-      
-    directories.each do |t|
+    repos.contents.get user: 'ServiceInnovationLab', repo: 'openfisca-aotearoa', path: "openfisca_aotearoa/#{dir}"
+  end
+
+  def self.scrape_all
+    dirs = connect('tests')
+
+    dirs.each do |t|
       puts "======================================================================"
-      puts t.url
+
+      if t.name.include?('.yaml')
+        puts "File: #{t.name}"
+      else
+        puts "Directory: #{t.name}"
+        children = connect("tests/#{t.name}")
+        children.each do |c|
+          puts c.name
+        end
+      end
     end
   end
 
