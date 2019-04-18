9ba32747d2a7c04c27fa87ce31ae451512b088f2
Mischa Saunders <mischa.saunders@gmail.com>
2019-04-18 12:44:06 +1200

    fix up stale scenario test

--- spec/services/scenarios_fetch_service.rb
+++ spec/services/scenarios_fetch_service.rb
@@ -5,20 +5,16 @@ require 'rails_helper'
 RSpec.describe ScenariosFetchService do
   let(:scenarios_total_number) { 5 }
 
-  let(:scenarios) do
-    FactoryBot.build_list(:scenario, scenarios_total_number)
-  end
-
   describe '.fetch_all' do
     it 'adds all of the Scenarios to the database' do
       expect { described_class.fetch_all }.to change { Scenario.count }.by(scenarios_total_number)
     end
 
-    xit 'removes stale Scenarios the database which are no longer part of the Data' do
+    it 'removes stale Scenarios the database which are no longer part of the Data' do
       described_class.fetch_all
-      stale_scenario, fresh_scenario = scenarios.sample(2)
-      scenarios.delete(stale_scenario.name)
-      described_class.fetch_all
+      stale_scenario, fresh_scenario = Scenario.all.sample(2)
+
+      described_class.remove_stale_scenarios(scenario_names: [fresh_scenario.name])
       expect(Scenario.find_by(name: stale_scenario.name)).to be_nil
       expect(Scenario.find_by(name: fresh_scenario.name)).not_to be_nil
     end
