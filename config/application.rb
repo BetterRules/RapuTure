a4127a92a4b69602baad3de5e7c200f0e82d2bd5
Brenda <brenda@wallace.net.nz>
2019-04-24 08:53:01 +1200

    Removing coffee

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module AwhiMai
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.generators.javascript_engine = :js
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
