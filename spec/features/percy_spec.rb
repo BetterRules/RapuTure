39b7649a8391bc09bf4fcbf9b13254df1c409532
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-04-24 16:08:13 +1200

    Add data to home page in percy test

--- spec/features/percy_spec.rb
+++ spec/features/percy_spec.rb
@@ -4,7 +4,10 @@ require 'rails_helper'
 require 'percy'
 
 describe 'Test with visual testing', type: :feature, js: true do
-  it 'loads example.com homepage' do
+  it 'loads homepage' do
+    person = FactoryBot.create :entity, name: 'person'
+    FactoryBot.create :variable, name: 'first_variable', entity: person
+    FactoryBot.create :variable, name: 'second_variable', entity: person
     visit root_path
     Percy.snapshot(page)
   end
