ec36127b5ffccde2dd0a89d5d675870403bada38
Brenda <brenda@wallace.net.nz>
2019-04-24 13:20:27 +1200

    lint

--- spec/factories/scenario.rb
+++ spec/factories/scenario.rb
@@ -4,10 +4,10 @@ FactoryBot.define do
   factory :scenario do
     name { Faker::Name.name }
     inputs { Faker::Json }
-    period { Faker::Name.name}
-    error_margin { Faker::Number.number}
+    period { Faker::Name.name }
+    error_margin { Faker::Number.number }
     created_at { DateTime.now.utc }
     updated_at { DateTime.now.utc }
-    namespace {Faker::Quote.yoda}
+    namespace { Faker::Quote.yoda }
   end
 end
