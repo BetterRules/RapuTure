45fc156e10898f87cfe302f48637dbf384b3e5f7
Dana Iti <dana.iti.work@gmail.com>
2019-04-29 16:25:22 +1200

    refactor github scraper

--- app/services/github_scrape_service.rb
+++ app/services/github_scrape_service.rb
@@ -1,38 +1,45 @@
 # frozen_string_literal: true
 
 class GithubScrapeService
-  # TODO: check if scenarios folder exists
-  FILES = { name: 'Files', include: ['.yaml', 'tests/', 'blob/master'], exclude: ['&source=login'] }.freeze
   SCENARIOS_DIR = './app/scenarios'
+  RAW_FILE_DIR = 'https://raw.githubusercontent.com/ServiceInnovationLab/openfisca-aotearoa/master/'
+  TOP_LEVEL_INCLUDE = 'tree/master/openfisca_aotearoa/tests/'
+  SECOND_LEVEL_INCLUDE = 'blob/master/openfisca_aotearoa/tests/'
+  SECOND_LEVEL_STRIP_URL = 'https://github.com/ServiceInnovationLab/openfisca-aotearoa/tree/master/openfisca_aotearoa/tests/'
+  RAW_FILE_STRIP_URL = 'https://github.com/ServiceInnovationLab/openfisca-aotearoa/blob/master/'
 
   def self.scrape_all
     clean_dir
-    puts 'Scraping openfisca-aotearoa from Github...'
+
+    # Scrape first level directory
     page = MetaInspector.new(ENV['GITHUB_URL'] + ENV['GITHUB_TESTS_PATH'])
-    collect_urls(page)
+
+    # get all links
+    page.links.all.each do |link|
+      next unless link.include?(TOP_LEVEL_INCLUDE)
+
+      files = MetaInspector.new(link, allow_non_html_content: false)
+      files.links.all.each do |file|
+        next unless file.include?(SECOND_LEVEL_INCLUDE)
+
+        link.gsub!(SECOND_LEVEL_STRIP_URL, '')
+        directory = "#{SCENARIOS_DIR}/#{link}"
+
+        # get file contents
+        raw_file = file.gsub!(RAW_FILE_STRIP_URL, RAW_FILE_DIR)
+        raw_file_contents = MetaInspector.new(raw_file, allow_non_html_content: true)
+
+        mkfile = "#{directory}/#{file.split('/').last}"
+
+        mkdir_mkfile(directory, mkfile, raw_file_contents)
+      end
+    end
   end
 
-  def self.collect_urls(page)
-    all_links = []
-    all_dirs = []
-
-    # TODO: Too much repitition, ideas on to how refactor this are welcome!
-    files(page, FILES).each do |link|
-      all_links.push(link)
-    end
-    dirs(page).each do |link|
-      all_dirs.push(link)
-    end
-    all_dirs.each do |dir|
-      page = MetaInspector.new(dir)
-      files(page, FILES).each do |link|
-        all_links.push(link)
-      end
-      dirs(page).each do |link|
-        all_dirs.push(link)
-      end
-    end
-    write_files(all_links)
+  def self.mkdir_mkfile(dir, file, content)
+    FileUtils.mkdir_p dir
+    FileUtils.touch file
+    File.open(file, 'w+') { |file| file.write(content) }
   end
 
   def self.clean_dir
@@ -42,59 +49,5 @@ class GithubScrapeService
         puts 'Scenario files exist! Cleaning scenarios directory.'
       end
     end
-  end
-
-  def self.write_files(all_links)
-    all_links.uniq.each do |link|
-      file_name = link.split('/').last
-      folder = link.split('/')[-2]
-      tidy_url(link)
-      page = MetaInspector.new(link, allow_non_html_content: true)
-      write_to_dir(file_name, page, folder)
-    end
-  end
-
-  def self.tidy_url(link)
-    link.gsub!('https://github.com', 'https://raw.githubusercontent.com')
-    link.gsub!('blob/', '')
-    link
-  end
-
-  def self.files(page, filters)
-    results = []
-    page.links.all.each do |link|
-      if included_in?(link, filters[:include]) && !included_in?(link, filters[:exclude])
-        results.push(link)
-        puts "Added File: #{link}"
-      end
-    end
-    results
-  end
-
-  def self.included_in?(link, array)
-    array.all? { |i| link.include?(i) }
-  end
-
-  def self.dirs(page)
-    results = []
-    page.links.all.each do |link|
-      # TODO: work out why self.included_in? does not work here?....
-
-      # TODO: need to check if directory has a yaml file or not. if not go to next directory (for income_tax/)
-      # TODO: income_tax not working, need workaround
-      if !link.include?('https://github.com/login') && !link.include?('.yaml') && !link.include?('income_tax') && link.include?('tests/') && !link.include?('#start-of-content') && link.include?('tree/master/') && !link.include?('&source=login')
-        results.push(link)
-      end
-    end
-    results
-  end
-
-  def self.write_to_dir(file_name, page, folder)
-    dir = "app/scenarios/#{folder}"
-    file = "app/scenarios/#{folder}/#{file_name}"
-
-    Dir.mkdir(dir) unless Dir.exist?(dir)
-
-    File.open(file, 'w+') { |f| f.write(page) } unless File.exist?(file)
   end
 end
