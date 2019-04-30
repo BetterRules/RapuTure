aff1de69f7ae87014b3232fd63d42a47fc0e55f8
Brenda <brenda@wallace.net.nz>
2019-04-30 14:08:40 +1200

    Rubocop lint tidy

--- spec/features/percy_spec.rb
+++ spec/features/percy_spec.rb
@@ -8,20 +8,20 @@ describe 'Test with visual testing', type: :feature, js: true do
   let!(:person) { FactoryBot.create :entity, name: 'person', documentation: 'this is a human being' }
   let(:variable) do
     FactoryBot.create :variable, entity: person,
-      name: 'is_eligible_for_chocolate',
-      description: 'is eligible for chocolate',
-      namespace: 'chocolate',
-      value_type: value_type
+                                 name: 'is_eligible_for_chocolate',
+                                 description: 'is eligible for chocolate',
+                                 namespace: 'chocolate',
+                                 value_type: value_type
   end
   before do
     FactoryBot.create :variable,
-      name: 'first_variable', entity: person, description: 'this one was first',
-        namespace: 'humans',
-        value_type: value_type
+                      name: 'first_variable', entity: person, description: 'this one was first',
+                      namespace: 'humans',
+                      value_type: value_type
     FactoryBot.create :variable,
-      name: 'second_variable', entity: person, description: 'and this one was second',
-        namespace: 'humans',
-        value_type: value_type
+                      name: 'second_variable', entity: person, description: 'and this one was second',
+                      namespace: 'humans',
+                      value_type: value_type
   end
   it 'loads homepage' do
     visit root_path
