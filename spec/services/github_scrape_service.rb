be51e7cf3c9d60f445f1197ed5d1a387f13276b3
Dana Iti <dana.iti.work@gmail.com>
2019-05-01 12:06:23 +1200

    more tests

--- spec/services/github_scrape_service.rb
+++ spec/services/github_scrape_service.rb
@@ -27,20 +27,34 @@ RSpec.describe GithubScrapeService do
     end
   end
 
-  it 'cleans the scenarios directory' do
-    described_class.clean_dir
-    expect(FileUtils.rm_rf(Dir.glob("#{ENV['SCENARIOS_DIR']}/*"))).to match_array([])
-  end
-
-  it 'returns the directory name for a scenario' do
-    link = 'citizenship'
-    described_class.top_level_url(link)
-    expect(described_class.top_level_url(link)).to eq('./tmp/scenarios/citizenship')
-  end
-
   it 'returns all links on page' do
     page = MetaInspector.new(ENV['GITHUB_URL'] + ENV['GITHUB_TESTS_PATH'])
     described_class.get_page_links(page)
     expect(described_class.get_page_links(page)).not_to be_empty
+  end
+
+  xit 'gets the data from openfisca-aoteroa by given directory' do
+
+  end
+
+  it 'returns the directory name for a scenario' do
+    link = 'citizenship'
+    described_class.get_directory_name(link)
+    expect(described_class.get_directory_name(link)).to eq('./tmp/scenarios/citizenship')
+  end
+
+  it 'creates the directory and writes tests to file' do
+    dir = 'citizenship'
+    file = 'citizenship_adding_days'
+    content = ''
+    # described_class.mkdir_mkfile(dir, file, content)
+    expect(File).to be_directory(dir)
+    expect(File.file?("#{@main_dir}/#{dir}/#{file}.yaml")).to be true
+    expect(File.readlines("#{@main_dir}/#{dir}/#{file}.yaml")).not_to eq(content)
+  end
+
+  it 'cleans the scenarios directory' do
+    described_class.clean_dir
+    expect(FileUtils.rm_rf(Dir.glob("#{ENV['SCENARIOS_DIR']}/*"))).to match_array([])
   end
 end
