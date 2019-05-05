5756ad92c94fa83ef0aaf2c6ff0442db0234f2fe
Brenda <brenda@wallace.net.nz>
2019-05-06 11:24:16 +1200

    Move input/output yaml parsing to scenario model
    so it can be called outside the fetch service

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -5,4 +5,33 @@ class Scenario < ApplicationRecord
   has_many :variables, through: :scenario_variables, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
+
+  def parse_variables!
+    input_keys = get_all_keys(inputs)
+    output_keys = get_all_keys(outputs)
+    input_output_keys = input_keys + output_keys
+
+    # byebug
+    # # ensure they exist
+    # input_output_keys.each do |variable_name|
+    #   byebug
+    #   Variable.find_or_create_by(name: variable_name)
+    # end
+    self.variables = Variable.where(name: input_output_keys)
+  end
+
+  private
+
+  # This could use some refinement
+  # Currently it is returning all the keys
+  # Can't just user lower level though because of data structures like:
+  #  output:
+  #   best_start__tax_credit_per_child:
+  #     2018-07: [0, 0, 0, 260 ]
+  def get_all_keys(hash)
+    hash.each_with_object([]) do |(k, v), keys|
+      keys << k
+      keys.concat(get_all_keys(v)) if v.is_a? Hash
+    end
+  end
 end
--- app/services/scenarios_fetch_service.rb
+++ app/services/scenarios_fetch_service.rb
@@ -66,20 +66,10 @@ class ScenariosFetchService
       scenario.period = yaml_scenario['period']
       scenario.error_margin = yaml_scenario['absolute_error_margin']
       scenario.save!
-      scenario.variables = fetch_associated_variables(yaml_scenario['input'], yaml_scenario['output'])
+      scenario.parse_variables!
+      # scenario.variables = fetch_associated_variables(yaml_scenario['input'], yaml_scenario['output'])
       scenario
     end
-  end
-
-  def self.fetch_associated_variables(inputs, outputs)
-    input_keys = get_all_keys(inputs)
-    output_keys = get_all_keys(outputs)
-    input_output_keys = input_keys + output_keys
-    # ensure they exist
-    input_output_keys.each do |variable_name|
-      Variable.find_or_create_by(name: variable_name)
-    end
-    Variable.where(name: input_output_keys)
   end
 
   # https://gist.github.com/naveed-ahmad/8f0b926ffccf5fbd206a1cc58ce9743e
@@ -99,27 +89,5 @@ class ScenariosFetchService
       .where
       .not(name: scenario_names)
       .destroy_all
-  end
-
-  # Needs to be updated to use github scraper
-  def self.scrapped_data
-    # Faraday.new ENV['OPENFISCA_URL'] do |conn|
-    #   conn.response :json, content_type: /\bjson$/
-    #   conn.adapter Faraday.default_adapter
-    # end
-  end
-
-  # This could use some refinement
-  # Currently it is returning all the keys
-  # Can't just user lower level though because of data structures like:
-  #  output:
-  #   best_start__tax_credit_per_child:
-  #     2018-07: [0, 0, 0, 260 ]
-
-  def self.get_all_keys(hash)
-    hash.each_with_object([]) do |(k, v), keys|
-      keys << k
-      keys.concat(get_all_keys(v)) if v.is_a? Hash
-    end
   end
 end
