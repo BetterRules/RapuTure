f846c9377e6538ccf224c38bb66cfe2d7ac88c43
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-05-05 13:07:13 +1200

    Added more set values to entity record in percy spec
    So they don't change every run by using faker data

--- spec/features/percy_spec.rb
+++ spec/features/percy_spec.rb
@@ -5,7 +5,12 @@ require 'percy'
 
 describe 'Test with visual testing', type: :feature, js: true do
   let(:value_type) { FactoryBot.create :value_type, name: 'int' }
-  let!(:person) { FactoryBot.create :entity, name: 'person', documentation: 'this is a human being' }
+  let!(:person) do
+    FactoryBot.create :entity,
+      name: 'person',
+      description: 'this is a human being',
+      documentation: 'you, me, everyone'
+  end
   let!(:scenario) do
     FactoryBot.create :scenario,
       name: 'a complicated situation',
