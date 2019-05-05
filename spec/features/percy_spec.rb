7b81f4061101ea50dd822a7f5c0b15fbc6c6f1cc
Brenda <brenda@wallace.net.nz>
2019-05-05 21:47:41 +1200

    Fix percy spec

--- spec/features/percy_spec.rb
+++ spec/features/percy_spec.rb
@@ -7,18 +7,18 @@ describe 'Test with visual testing', type: :feature, js: true do
   let(:value_type) { FactoryBot.create :value_type, name: 'int' }
   let!(:person) do
     FactoryBot.create :entity,
-      name: 'person',
-      description: 'this is a human being',
-      documentation: 'you, me, everyone'
+                      name: 'person',
+                      description: 'this is a human being',
+                      documentation: 'you, me, everyone'
   end
   let!(:scenario) do
     FactoryBot.create :scenario,
-      name: 'a complicated situation',
-      inputs: '{"persons"=>{"fulltime_uni_student"=>{"age"=>21, "is_nz_citizen"=>true, "social_security__is_ordinarily_resident_in_new_zealand"=>true, "student_allowance__is_tertiary_student"=>true, "student_allowance__is_enrolled_fulltime"=>true, "student_allowance__meets_attendance_and_performance_requirements"=>true}, "Parttime_student"=>{"age"=>18, "is_nz_citizen"=>true, "social_security__is_ordinarily_resident_in_new_zealand"=>true, "student_allowance__is_tertiary_student"=>true, "student_allowance__approved_to_study_parttime"=>true}, "Overseas_student"=>{"age"=>27, "is_nz_citizen"=>true, "social_security__is_ordinarily_resident_in_new_zealand"=>true, "student_allowance__is_tertiary_student"=>true, "student_allowance__approved_to_study_overseas"=>true}, "Refugee"=>{"age"=>25, "immigration__is_recognised_refugee"=>true, "student_allowance__is_tertiary_student"=>true, "student_allowance__is_enrolled_fulltime"=>true, "student_allowance__meets_attendance_and_performance_requirements"=>true}, "Not_a_student"=>{"age"=>50, "is_nz_citizen"=>true, "social_security__is_ordinarily_resident_in_new_zealand"=>true, "student_allowance__is_tertiary_student"=>false}}, "families"=>{"Whanau"=>{"others"=>["fulltime_uni_student", "Overseas_student", "Refugee", "Not_a_student", "Parttime_student"]}}, "titled_properties"=>{"whare"=>{"others"=>["fulltime_uni_student", "Overseas_student", "Refugee", "Not_a_student", "Parttime_student"]}}}',
-      outputs: '{"student_allowance__eligible_for_basic_grant"=>[true, true, true, true, false]}',
-      period: '2019-05',
-      error_margin: 1.0,
-      namespace: 'ghostchips'
+                      name: 'a complicated situation',
+                      inputs: { 'persons' => { 'fulltime_uni_student' => { 'age' => 21, 'is_nz_citizen' => true, 'social_security__is_ordinarily_resident_in_new_zealand' => true, 'student_allowance__is_tertiary_student' => true, 'student_allowance__is_enrolled_fulltime' => true, 'student_allowance__meets_attendance_and_performance_requirements' => true }, 'Parttime_student' => { 'age' => 18, 'is_nz_citizen' => true, 'social_security__is_ordinarily_resident_in_new_zealand' => true, 'student_allowance__is_tertiary_student' => true, 'student_allowance__approved_to_study_parttime' => true }, 'Overseas_student' => { 'age' => 27, 'is_nz_citizen' => true, 'social_security__is_ordinarily_resident_in_new_zealand' => true, 'student_allowance__is_tertiary_student' => true, 'student_allowance__approved_to_study_overseas' => true }, 'Refugee' => { 'age' => 25, 'immigration__is_recognised_refugee' => true, 'student_allowance__is_tertiary_student' => true, 'student_allowance__is_enrolled_fulltime' => true, 'student_allowance__meets_attendance_and_performance_requirements' => true }, 'Not_a_student' => { 'age' => 50, 'is_nz_citizen' => true, 'social_security__is_ordinarily_resident_in_new_zealand' => true, 'student_allowance__is_tertiary_student' => false } }, 'families' => { 'Whanau' => { 'others' => %w[fulltime_uni_student Overseas_student Refugee Not_a_student Parttime_student] } }, 'titled_properties' => { 'whare' => { 'others' => %w[fulltime_uni_student Overseas_student Refugee Not_a_student Parttime_student] } } },
+                      outputs: { 'student_allowance__eligible_for_basic_grant' => [true, true, true, true, false] },
+                      period: '2019-05',
+                      error_margin: 1.0,
+                      namespace: 'ghostchips'
   end
   let(:variable) do
     FactoryBot.create :variable, entity: person,
