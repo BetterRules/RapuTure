6b2e80ba06f564bf72afb559814bc343c264c3bc
Awesome Code <team@xinminlabs.com>
2019-05-08 18:56:40 +0000

    Auto corrected by following Format Ruby Code

--- spec/services/variables_fetch_service_spec.rb
+++ spec/services/variables_fetch_service_spec.rb
@@ -42,7 +42,8 @@ RSpec.describe VariablesFetchService do
 
   describe '.fetch_all' do
     it 'adds all of the Variables to the database' do
-      expect { described_class.fetch_all }.to change(Variable, :count).by(variables_total_number)
+      expect { described_class.fetch_all }.to change(Variable,
+                                                     :count).by(variables_total_number)
     end
 
     it 'removes stale Variables the database which are no longer part of API' do
