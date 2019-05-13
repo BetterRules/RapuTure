cf3f76bfc4ef7ffc7a57d801dbc85323d884e802
Dana Iti <dana.iti.work@gmail.com>
2019-05-13 15:22:46 +1200

    tidy up show view

--- app/helpers/parameters_helper.rb
+++ app/helpers/parameters_helper.rb
@@ -4,4 +4,8 @@ module ParametersHelper
   def filename_to_title(url)
     url.gsub('.yaml', '').split('_').map(&:capitalize).join(' ')
   end
+
+  def url_from_reference(ref)
+    ref.match(/https?:\/\/[\S]+/)
+  end
 end
