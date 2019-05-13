0b736675bcb2a5a256204e4d587c2a24e489c5df
Brenda <brenda@wallace.net.nz>
2019-05-13 14:27:38 +1200

    Distinguish between input and output variables in scenarios

--- app/models/scenario_variable.rb
+++ app/models/scenario_variable.rb
@@ -3,4 +3,7 @@
 class ScenarioVariable < ApplicationRecord
   belongs_to :scenario
   belongs_to :variable
+  validates :direction, presence: true, inclusion: { in: %w[input output], message: '%{value} is not a valid direction' }
+  scope :input, -> { where(direction: 'input')}
+  scope :output, -> { where(direction: 'output')}
 end
