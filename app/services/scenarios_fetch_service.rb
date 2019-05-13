9b8f2edbfe29b5d70a78cff29c3ae8909a13b177
Dana Iti <dana.iti.work@gmail.com>
2019-05-13 17:06:37 +1200

    revert find_all_duplicates method and remove module as no other class us using

--- app/services/scenarios_fetch_service.rb
+++ app/services/scenarios_fetch_service.rb
@@ -85,6 +85,18 @@ class ScenariosFetchService
     end
   end
 
+  # https://gist.github.com/naveed-ahmad/8f0b926ffccf5fbd206a1cc58ce9743e
+  def self.find_all_duplicates(array)
+    map = {}
+    dup = []
+    array.each do |v|
+      map[v] = (map[v] || 0) + 1
+
+      dup << v if map[v] == 2
+    end
+    raise StandardError.new("These scenarios have duplicate names: #{dup} !!!!!") if dup[0]
+  end
+
   def self.remove_stale_scenarios(scenario_names:)
     Scenario
       .where
