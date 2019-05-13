2a108576ce2bbea2feea9e89de77608f0b79e195
Dana Iti <dana.iti.work@gmail.com>
2019-05-13 14:32:02 +1200

    adding routes, views for parameters

---
+++ app/controllers/parameters_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class ParametersController < ApplicationController
+  def index
+    @parameters = Parameter.all.order(:filename)
+  end
+
+  def show
+    @parameter = Parameter.find(params[:id])
+    @brackets = @parameter.brackets unless @parameter.brackets.empty?
+    @values = @parameter.values unless @parameter.values.empty?
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :entities, only: %i[show index]
   resources :variables, only: %i[show index]
   resources :scenarios, only: %i[show index]
+  resources :parameters, only: %i[show index]
   # get "/:page" => "pages#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
