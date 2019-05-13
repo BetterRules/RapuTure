3f0545889213fec41bf35ac4ecd637110c51ddcd
Brenda <brenda@wallace.net.nz>
2019-05-13 14:32:47 +1200

    lint

--- app/models/scenario_variable.rb
+++ app/models/scenario_variable.rb
@@ -3,7 +3,9 @@
 class ScenarioVariable < ApplicationRecord
   belongs_to :scenario
   belongs_to :variable
-  validates :direction, presence: true, inclusion: { in: %w[input output], message: '%{value} is not a valid direction' }
-  scope :input, -> { where(direction: 'input')}
-  scope :output, -> { where(direction: 'output')}
+  validates :direction, presence: true, inclusion: {
+    in: %w[input output], message: '%{value} is not a valid direction'
+  }
+  scope :input, -> { where(direction: 'input') }
+  scope :output, -> { where(direction: 'output') }
 end
