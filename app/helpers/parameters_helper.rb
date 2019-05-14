8105be1a1d6b6c9b40e47a14b0ac6c3272eec880
Dana Iti <dana.iti.work@gmail.com>
2019-05-14 15:36:12 +1200

    added badge

--- app/helpers/parameters_helper.rb
+++ app/helpers/parameters_helper.rb
@@ -2,7 +2,11 @@
 
 module ParametersHelper
   def filename_to_title(url)
-    url.gsub('.yaml', '').split('_').map(&:capitalize).join(' ')
+    url.gsub('.yaml', '').split('/').last.titleize
+  end
+
+  def name_of_act(str)
+    str.split('/').pop(2).first.titleize
   end
 
   def url_from_reference(ref)
