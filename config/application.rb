94aecb45b7a96e877f33ff62b22da4c7d9b2422f
Dana Iti <dana.iti.work@gmail.com>
2019-05-14 15:48:04 +1200

    remove references to an unused module

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ Bundler.require(*Rails.groups)
 
 module AwhiMai
   class Application < Rails::Application
-    config.autoload_paths += %W[#{config.root}/lib]
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.generators.javascript_engine = :js
