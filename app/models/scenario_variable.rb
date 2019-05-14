8f7fbadc0ad693cfc64136a1ff6e64cb9a86b69b
Brenda Wallace <brenda@wallace.net.nz>
2019-05-14 13:38:08 +1200

    Prefer annotated tokens (like %<foo>s) over template tokens (like %{foo}).

--- app/models/scenario_variable.rb
+++ app/models/scenario_variable.rb
@@ -4,7 +4,7 @@ class ScenarioVariable < ApplicationRecord
   belongs_to :scenario
   belongs_to :variable
   validates :direction, presence: true, inclusion: {
-    in: %w[input output], message: '%{value} is not a valid direction'
+    in: %w[input output], message: '%<value>s is not a valid direction'
   }
   scope :input, -> { where(direction: 'input') }
   scope :output, -> { where(direction: 'output') }
