939dcc54d1a76cb6aea1ac3f36b87342645cea6a
Dana Iti <dana.iti.work@gmail.com>
2019-05-24 18:39:00 +1200

    tests for parameters_helper

--- spec/helpers/parameters_helper_spec.rb
+++ spec/helpers/parameters_helper_spec.rb
@@ -5,4 +5,5 @@ require 'rails_helper'
 RSpec.describe ParametersHelper do
   it { expect(filename_to_title('my_name_is_bob')).to eq 'My Name Is Bob' }
   it { expect(name_of_act('my/name/is/bob')).to eq 'Is' }
+  it { expect(github_file_url('./tmp/development-openfisca-aotearoa/openfisca_aotearoa/parameters/acc/weekly_compensation_abatement.yaml')).to eq 'https://github.com/ServiceInnovationLab/openfisca-aotearoa/blob/master/openfisca_aotearoa/parameters/acc/weekly_compensation_abatement.yaml' }
 end
